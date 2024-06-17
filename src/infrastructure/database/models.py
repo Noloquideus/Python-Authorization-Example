c69e9ff6f39e7991b9131eed5a6120e8e614c679
Noloquideus <daniilmanukian@gmail.com>
2024-06-17 23:47:03 +0300

    chore: add user model

---
+++ src/infrastructure/database/models.py
@@ -0,0 +1,16 @@
+import uuid
+from sqlalchemy import Column, UUID, String, Boolean, Integer
+from sqlalchemy.dialects.postgresql import ARRAY
+from src.infrastructure.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    username = Column(String, nullable=False, unique=True)
+    password_hash = Column(String, nullable=False)
+    access_level = Column(Integer, nullable=False, default=1)
+    is_email_verified = Column(Boolean, nullable=False, default=False)
+    refresh_tokens = Column(ARRAY(String), nullable=True)
