f3c032a22698be4b8f5272ae1a2f44f6619dc6b7
Noloquideus <daniilmanukian@gmail.com>
2024-06-17 23:31:45 +0300

    feat: add register endpoint

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,6 +1,21 @@
 from fastapi import APIRouter
+from starlette import status
 
 auth_router = APIRouter(
     prefix="/auth",
     tags=["auth"],
     responses={404: {"description": "Not found"}, 500: {"description": "Internal server error"}, 400: {"description": "Bad request"}})
+
+
+@auth_router.post(
+    path='/register',
+    status_code=status.HTTP_201_CREATED,
+    description='Register a new user by providing an email, username, and password.',
+    summary='Register a new user',
+    response_description='The newly registered user without the password',
+    responses={
+        status.HTTP_201_CREATED: {"description": "User successfully created"},
+        status.HTTP_400_BAD_REQUEST: {"description": "Invalid input data"},
+        status.HTTP_409_CONFLICT: {"description": "User already exists"}})
+async def register():
+    pass
