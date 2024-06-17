b6e8d8d82c83cec0fc1acc764d3171a48789b7b2
Noloquideus <daniilmanukian@gmail.com>
2024-06-17 23:49:07 +0300

    chore: implemented create_user method

--- src/infrastructure/database/repositories/user_repository.py
+++ src/infrastructure/database/repositories/user_repository.py
@@ -1,7 +1,13 @@
 from src.application.domain.user_domain import UserData
+from src.infrastructure.database.database import async_session_maker
+from src.infrastructure.database.models import User
 
 
 class UserRepository:
     @staticmethod
-    async def create_user(user_data: UserData):
-        pass
+    async def create_user(user_data: UserData) -> User:
+        async with async_session_maker() as session:
+            user_model = User(email=user_data.email, username=user_data.username, password_hash=user_data.password_hash)
+            session.add(user_model)
+            await session.commit()
+            return user_model
