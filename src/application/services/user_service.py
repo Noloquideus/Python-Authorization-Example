8d7ac33aee4f04fb9e1c46e50938291970239b33
Noloquideus <daniilmanukian@gmail.com>
2024-06-17 23:49:21 +0300

    chore: implemented create_user method

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -1,8 +1,12 @@
-from src.application.domain.user_domain import UserRegistration
+from src.application.domain.user_domain import UserRegistration, UserData
+from src.infrastructure.database.repositories.user_repository import UserRepository
+from src.infrastructure.utils.hash_service import HashService
 
 
 class UserService:
 
     @staticmethod
     async def create_user(user_data: UserRegistration):
-        pass
+        user_data = UserData(email=user_data.email, username=user_data.username, password_hash=HashService.hash(user_data.password))
+        user = await UserRepository.create_user(user_data)
+        return user
