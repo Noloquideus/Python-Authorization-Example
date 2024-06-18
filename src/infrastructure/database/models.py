0898d9bbe2bd7369282a07df98d1e06340026db9
Noloquideus <daniilmanukian@gmail.com>
2024-06-19 00:36:50 +0300

    feat: add index

--- src/infrastructure/database/models.py
+++ src/infrastructure/database/models.py
@@ -8,8 +8,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    username = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True, index=True)
+    username = Column(String, nullable=False, unique=True, index=True)
     password_hash = Column(String, nullable=False)
     access_level = Column(Integer, nullable=False, default=1)
     is_email_verified = Column(Boolean, nullable=False, default=False)
