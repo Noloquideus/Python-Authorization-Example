369c8b30fb62f6d4c725f4e448b7e45054dc12c3
Noloquideus <daniilmanukian@gmail.com>
2024-06-18 11:16:58 +0300

    feat: add settings to create access and refresh tokens

--- src/config.py
+++ src/config.py
@@ -13,6 +13,12 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    ALGORITHM: str
+    ACCESS_SECRET_KEY: str
+    ACCESS_TTL_MINUTES: int
+    REFRESH_SECRET_KEY: str
+    REFRESH_TTL_MINUTES: int
+
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
