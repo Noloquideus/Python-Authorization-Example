32fafd5983e9403c8bf57d171ba406a408ad77da
Noloquideus <daniilmanukian@gmail.com>
2024-06-18 23:30:41 +0300

    feat: add UserAlreadyExistsException

--- src/exceptions.py
+++ src/exceptions.py
@@ -4,3 +4,8 @@ from fastapi import HTTPException, status
 class DefaultException(HTTPException):
     def __init__(self, status_code: int, detail: str):
         super().__init__(status_code=status_code, detail=detail)
+
+
+class UserAlreadyExistsException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail="User with this email or username already exists")
