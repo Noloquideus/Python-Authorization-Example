d52513766c8d0358b96ee3810d5de131049e7960
Noloquideus <daniilmanukian@gmail.com>
2024-06-18 22:49:53 +0300

    feat: implementation create_user

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -1,12 +1,24 @@
-from src.application.domain.user_domain import UserRegistration, UserData
+from src.application.domain.jwt import RefreshToken
+from src.application.domain.user_domain import UserRegistration, UserData, UserDto, UserResponse
 from src.infrastructure.database.repositories.user_repository import UserRepository
 from src.infrastructure.utils.hash_service import HashService
+from src.infrastructure.utils.token_service import TokenService
 
 
 class UserService:
 
     @staticmethod
-    async def create_user(user_data: UserRegistration):
+    async def create_user(user_data: UserRegistration) -> UserResponse:
         user_data = UserData(email=user_data.email, username=user_data.username, password_hash=HashService.hash(user_data.password))
         user = await UserRepository.create_user(user_data)
-        return user
+
+        user_dto = UserDto(id=str(user.id),
+                           email=user.email,
+                           username=user.username,
+                           access_level=user.access_level,
+                           is_email_verified=user.is_email_verified)
+
+        tokens = RefreshToken(access_token=await TokenService.create_access_token(user=user_dto),
+                              refresh_token=await TokenService.create_refresh_token(user_id=user_dto.id))
+
+        return UserResponse(user=user_dto, tokens=tokens)
