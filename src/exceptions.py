757e0ff3adf05a92d4359cf4cff9ab093b71f1bd
Noloquideus <daniilmanukian@gmail.com>
2024-06-19 05:31:30 +0300

    feat: add UserNotFoundException

--- src/exceptions.py
+++ src/exceptions.py
@@ -19,3 +19,8 @@ class TokenTimeIsExpiredException(DefaultException):
 class InvalidTokenException(DefaultException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+
+class UserNotFoundException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
