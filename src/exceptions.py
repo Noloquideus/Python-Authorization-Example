05c8f7df0008b3142ec06a72cc89f7743ad41844
Noloquideus <daniilmanukian@gmail.com>
2024-06-19 05:23:57 +0300

    feat: add exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -9,3 +9,13 @@ class DefaultException(HTTPException):
 class UserAlreadyExistsException(DefaultException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_409_CONFLICT, detail="User with this email or username already exists")
+
+
+class TokenTimeIsExpiredException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token time is expired")
+
+
+class InvalidTokenException(DefaultException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
