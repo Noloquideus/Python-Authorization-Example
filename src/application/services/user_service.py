d114651115bc2b81cee4d0247a3bcabcf8b991ed
Noloquideus <daniilmanukian@gmail.com>
2024-06-19 17:38:24 +0300

    feat: delete tasks and send email

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -1,11 +1,10 @@
 from src.application.domain.jwt import RefreshToken, AccessToken
 from src.application.domain.user_domain import UserRegistration, UserData, UserDto, UserResponse
-from src.exceptions import UserNotFoundByEmailException, UserNotFoundByUsernameException
+from src.exceptions import UserNotFoundByEmailException
 from src.infrastructure.database.models import User
 from src.infrastructure.database.repositories.user_repository import UserRepository
 from src.infrastructure.utils.hash_service import HashService
 from src.infrastructure.utils.token_service import TokenService
-from src.infrastructure.tasks.tasks import send_confirmation_email
 
 
 class UserService:
@@ -22,10 +21,6 @@ class UserService:
 
         access_token = AccessToken(access_token=await TokenService.create_access_token(user=user_dto))
         refresh_token = RefreshToken(refresh_token=await TokenService.create_refresh_token(user_id=user_dto.id))
-
-        verify_token = await TokenService.create_verify_token(email=user_dto.email)
-        confirmation_link = f"http://localhost:7777/verify?token={verify_token}"
-        send_confirmation_email.delay(to=user.email, subject="Email Confirmation", body=confirmation_link)
 
         await UserRepository.add_refresh_token(user_id=user.id, refresh_token=refresh_token.refresh_token)
 
