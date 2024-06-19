4af7d7ca155e45973e7ba46b80e8e15fb18e231b
Noloquideus <daniilmanukian@gmail.com>
2024-06-19 05:11:29 +0300

    feat: send email with celery

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -3,10 +3,10 @@ from src.application.domain.user_domain import UserRegistration, UserData, UserD
 from src.infrastructure.database.repositories.user_repository import UserRepository
 from src.infrastructure.utils.hash_service import HashService
 from src.infrastructure.utils.token_service import TokenService
+from src.infrastructure.tasks.tasks import send_confirmation_email
 
 
 class UserService:
-
     @staticmethod
     async def create_user(user_data: UserRegistration) -> UserResponse:
         user_data = UserData(email=user_data.email, username=user_data.username, password_hash=HashService.hash(user_data.password))
@@ -20,6 +20,10 @@ class UserService:
 
         access_token = AccessToken(access_token=await TokenService.create_access_token(user=user_dto))
         refresh_token = RefreshToken(refresh_token=await TokenService.create_refresh_token(user_id=user_dto.id))
+
+        verify_token = await TokenService.create_verify_token(email=user_dto.email)
+        confirmation_link = f"http://localhost:7777/verify?token={verify_token}"
+        send_confirmation_email.delay(to=user.email, subject="Email Confirmation", body=confirmation_link)
 
         await UserRepository.add_refresh_token(user_id=user.id, refresh_token=refresh_token.refresh_token)
 
