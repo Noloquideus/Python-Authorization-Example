762c29da927382ea80cc69345f9a49679793697b
Noloquideus <daniilmanukian@gmail.com>
2024-06-20 15:56:20 +0300

    feat: add login method

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -1,10 +1,11 @@
 from src.application.domain.jwt import RefreshToken, AccessToken
-from src.application.domain.user_domain import UserRegistration, UserData, UserDto, UserResponse
-from src.exceptions import UserNotFoundByEmailException
+from src.application.domain.user_domain import UserRegistration, UserData, UserDto, UserResponse, UserLogin
+from src.exceptions import UserNotFoundByEmailException, UserNotFoundException, InvalidCredentialsException
 from src.infrastructure.database.models import User
 from src.infrastructure.database.repositories.user_repository import UserRepository
 from src.infrastructure.utils.hash_service import HashService
 from src.infrastructure.utils.token_service import TokenService
+from src.infrastructure.utils.validate_email import is_valid_email
 
 
 class UserService:
@@ -25,6 +26,37 @@ class UserService:
         await UserRepository.add_refresh_token(user_id=user.id, refresh_token=refresh_token.refresh_token)
 
         return UserResponse(user=user_dto, access_token=access_token, refresh_token=refresh_token)
+
+    @staticmethod
+    async def login(user_data: UserLogin) -> UserResponse:
+        if is_valid_email(user_data.login):
+            user = await UserRepository.get_user_by_email(user_data.login)
+        else:
+            user = await UserRepository.get_user_by_username(user_data.login)
+
+        if not user:
+            raise UserNotFoundException
+
+        if not HashService.verify(user.password_hash, user_data.password):
+            raise InvalidCredentialsException
+
+        user_dto = UserDto(id=str(user.id),
+                           email=user.email,
+                           username=user.username,
+                           access_level=user.access_level,
+                           is_email_verified=user.is_email_verified)
+
+        access_token = AccessToken(access_token=await TokenService.create_access_token(user=user_dto))
+        refresh_token = RefreshToken(refresh_token=await TokenService.create_refresh_token(user_id=user_dto.id))
+
+        await UserRepository.add_refresh_token(user_id=user.id, refresh_token=refresh_token.refresh_token)
+
+        return UserResponse(user=user_dto, access_token=access_token, refresh_token=refresh_token)
+
+
+
+
+
 
     @staticmethod
     async def verify_user(token: str) -> User:
