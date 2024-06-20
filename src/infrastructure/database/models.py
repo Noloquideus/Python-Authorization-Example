02e3a725e6cf3403558577cdacbb983829510847
Noloquideus <daniilmanukian@gmail.com>
2024-06-20 19:16:54 +0300

    feat: add index for id

--- src/infrastructure/database/models.py
+++ src/infrastructure/database/models.py
@@ -7,7 +7,7 @@ from src.infrastructure.database.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
     username = Column(String, nullable=False, unique=True, index=True)
     password_hash = Column(String, nullable=False)
