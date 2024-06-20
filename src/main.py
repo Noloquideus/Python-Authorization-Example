79edffe44723ffe637161ae916e5d70ef9c012d3
Noloquideus <daniilmanukian@gmail.com>
2024-06-20 16:17:02 +0300

    feat: add prometheus

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from src.api.auth import auth_router
 from src.infrastructure.cache.redis import init_redis
 
@@ -15,6 +16,11 @@ async def lifespan(_):
 app = FastAPI(title='Auth Service', description='Auth Service API', version='1.0.0', redoc_url=None, lifespan=lifespan)
 app.include_router(auth_router)
 
+instrumentator = Instrumentator(
+    should_group_status_codes=False,
+    excluded_handlers=[".*admin.*", "/metrics"],
+)
+instrumentator.instrument(app).expose(app)
 
 @app.get("/")
 async def root():
