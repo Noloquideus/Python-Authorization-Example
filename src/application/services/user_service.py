e2e0afb8775c8beb3f6b31889d0b9c80e0240dcd
Noloquideus <daniilmanukian@gmail.com>
2024-06-20 19:13:38 +0300

    feat: add create_tokens

--- src/application/services/user_service.py
+++ src/application/services/user_service.py
@@ -1,6 +1,8 @@
+from uuid import UUID
 from src.application.domain.jwt import RefreshToken, AccessToken
 from src.application.domain.user_domain import UserRegistration, UserData, UserDto, UserResponse, UserLogin
-from src.exceptions import UserNotFoundByEmailException, UserNotFoundException, InvalidCredentialsException
+from src.config import settings
+from src.exceptions import UserNotFoundByEmailException, UserNotFoundException, InvalidCredentialsException, TooManyRefreshTokensException
 from src.infrastructure.database.models import User
 from src.infrastructure.database.repositories.user_repository import UserRepository
 from src.infrastructure.utils.hash_service import HashService
@@ -53,11 +55,6 @@ class UserService:
 
         return UserResponse(user=user_dto, access_token=access_token, refresh_token=refresh_token)
 
-
-
-
-
-
     @staticmethod
     async def verify_user(token: str) -> User:
         email = await TokenService.decode_verify_token(token)
@@ -67,3 +64,33 @@ class UserService:
         user.is_email_verified = True
         await UserRepository.update_user(user)
         return user
+
+    @staticmethod
+    async def create_tokens(refresh_token: str) -> UserResponse:
+        token_data = await TokenService.decode_refresh_token(refresh_token)
+        user_id = UUID(token_data["sub"])
+        user = await UserRepository.get_user_by_id(user_id)
+
+        if user is None:
+            await UserRepository.delete_all_refresh_tokens(user_id=user_id)
+            raise UserNotFoundException
+
+        if refresh_token not in user.refresh_tokens:
+            await UserRepository.delete_all_refresh_tokens(user_id=user_id)
+            raise InvalidCredentialsException
+
+        if len(user.refresh_tokens) > settings.MAX_REFRESH_TOKENS_IN_DB:
+            await UserRepository.delete_all_refresh_tokens(user_id=user_id)
+            raise TooManyRefreshTokensException
+
+        user_dto = UserDto(id=str(user_id),
+                           email=user.email,
+                           username=user.username,
+                           access_level=user.access_level,
+                           is_email_verified=user.is_email_verified)
+
+        refresh_token = RefreshToken(refresh_token=await TokenService.create_refresh_token(user_id=user_dto.id))
+        access_token = AccessToken(access_token=await TokenService.create_access_token(user=user_dto))
+        await UserRepository.add_refresh_token(user_id=user_id, refresh_token=refresh_token.refresh_token)
+
+        return UserResponse(user=user_dto, access_token=access_token, refresh_token=refresh_token)
